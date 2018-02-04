07ddd01c81bbcf25d6fd778ca591f591e01843b3
Aman020 <amanbhayana@yahoo.in>
2018-02-04 13:39:06 +0530

    First Commit

---
+++ CRUD-CosmosDb.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace CRUD_CosmosDb.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ CRUD-CosmosDb.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CRUD_CosmosDb.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
